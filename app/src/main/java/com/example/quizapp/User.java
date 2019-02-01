3abd10b64737c41f2d000b51a830387a51e10bee
shreya <shreyadwivedi15@gmail.com>
2019-02-01 14:19:00 +0530

    commit with follow API

--- app/src/main/java/com/example/quizapp/User.java
+++ app/src/main/java/com/example/quizapp/User.java
@@ -2,11 +2,16 @@ package com.example.quizapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.quizapp.api.IConnectAPI;
+import com.example.quizapp.models.response.Follow;
+import com.example.quizapp.models.response.FollowResponse;
 import com.example.quizapp.models.response.UserProfileResponse;
 
 import java.text.SimpleDateFormat;
@@ -22,7 +27,9 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class User extends AppCompatActivity {
     ImageView userdp;
     TextView followers, following, interest, about, name, dob;
+    Button follow;
     String userId = "c2b67514-cc58-42d1-904a-3b6083ecd6de";
+    String followerId="8a8a7a69-b642-4f7f-a4ef-2b43a370c3fe";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +42,7 @@ public class User extends AppCompatActivity {
         about = findViewById(R.id.about);
         name = findViewById(R.id.name);
         dob = findViewById(R.id.DOB);
+        follow=findViewById(R.id.followBtn);
 
         OkHttpClient client = new OkHttpClient.Builder().build();
         //http://10.0.2.2:8000 or http://localhost:8000
@@ -66,7 +74,44 @@ public class User extends AppCompatActivity {
                 System.out.println("error" + t.getLocalizedMessage());
             }
         });
+
+        follow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                OkHttpClient client1 = new OkHttpClient.Builder().build();
+                //http://10.0.2.2:8000 or http://localhost:8000
+                final Retrofit retrofit = new Retrofit.Builder()
+                        .baseUrl("http://10.177.7.124:8081")
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .client(client1)
+                        .build();
+                IConnectAPI iApi1 = retrofit.create(IConnectAPI.class);
+                Follow follow=new Follow();
+                follow.setUserId(userId);
+                follow.setFollowerId(followerId);
+                iApi1.follow(follow).enqueue(new Callback<FollowResponse>() {
+                    @Override
+                    public void onResponse(Call<FollowResponse> call, Response<FollowResponse> response) {
+                       if (response.isSuccessful()){
+                        Toast.makeText(User.this, "You are now following this user", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                           Toast.makeText(User.this, "couldn't follow, try again", Toast.LENGTH_SHORT).show();
+                    }}
+
+                    @Override
+                    public void onFailure(Call<FollowResponse> call, Throwable t) {
+                        Toast.makeText(User.this, "BACKEND ISSUE", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+            }
+        });
     }
+
+
 
 
 }
--- app/src/main/java/com/example/quizapp/api/IConnectAPI.java
+++ app/src/main/java/com/example/quizapp/api/IConnectAPI.java
@@ -1,6 +1,8 @@
 package com.example.quizapp.api;
 
 import com.example.quizapp.adapter.LikeDislikePost;
+import com.example.quizapp.models.response.Follow;
+import com.example.quizapp.models.response.FollowResponse;
 import com.example.quizapp.models.response.PostListItem;
 import com.example.quizapp.models.response.UserFeedResponse;
 import com.example.quizapp.models.response.UserProfileResponse;
@@ -8,6 +10,7 @@ import com.example.quizapp.models.response.UserProfileResponse;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
@@ -21,6 +24,8 @@ public interface IConnectAPI {
     public Call<UserProfileResponse> getUserInfo(@Path("userId")String userId);
     @POST("/post/like/{postId}/{userId}")
     public Call<LikeDislikePost> postLike(@Path("postId") String postId, @Path("userId") String userId);
+    @POST("/carts/add")
+    public Call<FollowResponse> follow(@Body Follow follow);
 
     @DELETE("/post/dislike/{postId}/{userId}")
     public Call<LikeDislikePost> postDislike(@Path("postId") String postId, @Path("userId") String userId);

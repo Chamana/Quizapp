2609c6bee4c85f8757ef65a1aea5c5dc38f382ea
nupurtandon <nupstan@gmail.com>
2019-02-01 12:44:27 +0530

    advertisements notifications, likes

---
+++ app/src/main/java/com/example/quizapp/response/LikeDislikePost.java
@@ -0,0 +1,49 @@
+package com.example.quizapp.response;
+
+import java.util.List;
+import com.google.gson.annotations.SerializedName;
+
+public class LikeDislikePost{
+
+	@SerializedName("message")
+	private String message;
+
+	@SerializedName("statusCode")
+	private int statusCode;
+
+	@SerializedName("status")
+	private boolean status;
+
+	public void setMessage(String message){
+		this.message = message;
+	}
+
+	public String getMessage(){
+		return message;
+	}
+
+	public void setStatusCode(int statusCode){
+		this.statusCode = statusCode;
+	}
+
+	public int getStatusCode(){
+		return statusCode;
+	}
+
+	public void setStatus(boolean status){
+		this.status = status;
+	}
+
+	public boolean isStatus(){
+		return status;
+	}
+
+	@Override
+	public String toString() {
+		return "LikeDislikePost{" +
+				"message='" + message + '\'' +
+				", statusCode=" + statusCode +
+				", status=" + status +
+				'}';
+	}
+}
